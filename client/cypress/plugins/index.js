1428f37e49a56c8f48d570f8481688e6f5f85a60
Gabriel Dutra <nesk.frz@gmail.com>
2020-09-03 14:50:34 -0300

    Test disabling code coverage

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,8 +1,8 @@
 const percyHealthCheck = require("@percy/cypress/task"); // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
 
-module.exports = (on, config) => {
-  require("@cypress/code-coverage/task")(on, config);
+module.exports = on => {
+  // require("@cypress/code-coverage/task")(on, config);
   on("task", percyHealthCheck);
 
-  return config;
+  // return config;
 };
